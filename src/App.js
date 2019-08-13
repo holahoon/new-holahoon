7ba2880e71f7237de8a076ea5156afa71a129f7e
David Kim <rtdk0324@gmail.com>
2019-08-13 11:21:01 -0400

    Added Google Analytics

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 // import { CSSTransition, TransitionGroup } from "react-transition-group";
+import ReactGA from "react-ga";
 
 import Layout from "./components/Layout/Layout";
 import MainPage from "./containers/MainPage/MainPage";
@@ -8,7 +9,25 @@ import Projects from "./components/Projects/Projects";
 
 import "./App.css";
 
+ReactGA.pageview(window.location.pathname + window.location.search);
+
 class App extends Component {
+  componentDidMount() {
+    this.initGA();
+    this.logPageView();
+  }
+
+  // Google Analytics
+  initGA = () => {
+    console.log("GA init");
+    ReactGA.initialize("UA-145620488-1");
+  };
+
+  logPageView = () => {
+    ReactGA.set({ page: window.location.pathname });
+    ReactGA.pageview(window.location.pathname);
+  };
+
   render() {
     return (
       <BrowserRouter>
